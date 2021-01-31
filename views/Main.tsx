010449fa93e971e025bf0b11dd270a8901e2acfa
Leonardo <sciretta28@hotmail.com>
2021-01-31 10:42:11 -0400

    Little issue

--- views/Main.tsx
+++ views/Main.tsx
@@ -1,7 +1,5 @@
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
-import Avatar from '@material-ui/core/Avatar'
-
 import Contact from 'components/Contact'
 import useStyles from 'styles/ViewsStyles'
 import Image from 'next/image'
