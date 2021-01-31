7dd7401590ecf6a0a0297c7a952d5711074b444a
Leonardo <sciretta28@hotmail.com>
2021-01-31 11:09:37 -0400

    Loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,25 @@
 import Head from 'next/head'
-// import {Suspense,lazy} from 'react'
 import { ThemeProvider } from '@material-ui/core/styles'
-// import Skeleton from '@material-ui/lab/Skeleton'
+import Skeleton from '@material-ui/lab/Skeleton'
 import { lightTheme } from 'styles/Theme'
 import Layout from 'views/Layout'
-
-// const Main = lazy(() => import('views/Main'))
-// const Fronted = lazy(() => import('views/Frontend'))
-// const Backend = lazy(() => import('views/Backend'))
 
 import Main from 'views/Main'
 import Frontend from 'views/Frontend'
 import Backend from 'views/Backend'
 import Experience from 'views/Experience'
+import { useEffect, useState } from 'react'
 
 
 export default function Home() {
+  const [ loading , setLoading ] = useState(true)
+
+  useEffect(()=>{
+    setLoading(false)
+  },[])
+
+  if(loading) return <Loading/>
+
   return (
     <ThemeProvider theme={lightTheme}>
       <Head>
@@ -23,11 +27,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        {/* <Suspense fallback={<Loading/>}>
-	        <Main/>
-	        <Frontend/>
-	        <Backend/>
-	      </Suspense> */}
         <Main/>
         <Frontend/>
         <Backend/>
@@ -37,19 +36,20 @@ export default function Home() {
   )
 }
 
-// function Loading(){
-//   return(
-// 	  <Skeleton variant="rect" width={500} height={60}
-// 	  style={{
-// 	  	marginTop:'15vh',
-// 	  	backgroundColor:'rgba(0,0,0,.7)',
-// 	  	borderRadius:'5px',
-// 	  	color:'lightgrey',
-// 	  	fontSize:'20px',
-// 	  	fontWeight:'bold',
-// 	  	display:'flex',
-// 	  	alignItems:'center',
-// 	  	justifyContent:'center'
-// 	  }}>Loading...</Skeleton>
-// 	)
-// }
+function Loading(){
+  return(
+	  <Skeleton variant="rect" width={500} height={60}
+	  style={{
+	  	marginTop:'15vh',
+	  	backgroundColor:'rgba(0,0,0,.7)',
+	  	borderRadius:'5px',
+	  	color:'lightgrey',
+	  	fontSize:'20px',
+	  	fontWeight:'bold',
+	  	display:'flex',
+	  	alignItems:'center',
+      justifyContent:'center',
+      width:'100vw'
+	  }}>Loading...</Skeleton>
+	)
+}
