b0f897ad72a0d4162490db105a328d4947fdf567
Leonardo <sciretta28@hotmail.com>
2021-03-15 00:24:02 -0400

    size of the tec card

--- views/Frontend.tsx
+++ views/Frontend.tsx
@@ -20,7 +20,7 @@ export default function Fronted() {
           variant="h1"
           className={classes.text}
         >
-          Fronted skills✌:
+          Frontend skills✌:
         </Typography>
         <Grid 
           item container
