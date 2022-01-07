1c0f7cdde580b4a25280803d9ee751ac882abf2b
Leonardo <sciretta28@gmail.com>
2022-01-06 22:11:27 -0400

    reset to 0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,9 @@
-import loadable from '@loadable/component'
-import Head from 'next/head'
-import { ThemeProvider } from '@material-ui/core/styles'
-import { lightTheme } from 'styles/Theme'
-import { Loading } from 'components/Loading'
 
-const Layout = loadable(() => import('views/Layout'))
-const Main = loadable(() => import('views/Main'))
-const Frontend = loadable(() => import('views/Frontend'))
-const General = loadable(() => import('views/General'))
-const Backend = loadable(() => import('views/Backend'))
-const Experience = loadable(() => import('views/Experience'))
+// const Main = loadable(() => import('views/Main'))
+// <Main fallback={<Loading display="main section 👋" />} /> 
 
 export default function Home() {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <Head>
-        <title>Leonardo Sciretta</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        <Main fallback={<Loading display="main section 👋" />} />
-        <Experience fallback={<Loading display="experience 💪" />} />
-        <General fallback={<Loading display="general skills 📖" />} />
-        <Frontend fallback={<Loading display="frontend skills ✌" />} />
-        <Backend fallback={<Loading display="backend skills 👌" />} />
-      </Layout>
-    </ThemeProvider>
+    <>index</>
   )
 }
