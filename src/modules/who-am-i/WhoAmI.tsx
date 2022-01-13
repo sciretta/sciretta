e43c11259cf9fbe1fd79a10122791316ad69bdaa
sciretta <sciretta28@gmail.com>
2022-01-13 19:59:32 -0400

    whoIAm section message

--- src/modules/who-am-i/WhoAmI.tsx
+++ src/modules/who-am-i/WhoAmI.tsx
@@ -1,32 +1,37 @@
 import Typewriter from 'typewriter-effect'
 
 const primaryFontStyle =
-  'color: #59899D;font-size: 30px; font-family: Open Sans'
+  'color: #ADBECC; font-family: Open Sans, Helvetica; font-weight: 500; font-size: 1.875rem;'
 
 function WhoAmI() {
   return (
     <div className="h-screen min-h-min bg-dark flex justify-center items-center flex-col">
-      <div>Hi im a Leonardo Sciretta</div>
-      <Typewriter
-        onInit={(typewriter) => {
-          typewriter
+      <div className="text-center">
+        <div className="text-lighter text-3xl font-medium text-3xl font-body mb-10">
+          Hello I am
+        </div>
+        <div className="text-lighter text-3xl font-medium text-6xl font-body mb-10">
+          Leonardo Sciretta
+        </div>
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
 
-            .typeString(
-              `<span style="${primaryFontStyle}">TypeScript and JavaScript developer.</span>`,
-            )
-            .pauseFor(3000)
-            .deleteAll()
-            .typeString(
-              `<span style="${primaryFontStyle}">Web and Mobile developer.</span>`,
-            )
-            .pauseFor(10000)
-            .start()
-        }}
-        options={{
-          loop: true,
-          cursor: `<span style="${primaryFontStyle}">|</span>`,
-        }}
-      />
+              .typeString(
+                `<span style="${primaryFontStyle}">TypeScript, JavaScript ...</span>`,
+              )
+              .pauseFor(3000)
+              .deleteAll(130)
+              .typeString(
+                `<span style="${primaryFontStyle}">Web and Mobile developer.</span>`,
+              )
+              .start()
+          }}
+          options={{
+            cursor: `<span style="${primaryFontStyle}">|</span>`,
+          }}
+        />
+      </div>
     </div>
   )
 }
