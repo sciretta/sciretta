2ca68ec4989f5f3a528a1e45327944d863834208
sciretta <sciretta28@gmail.com>
2022-01-13 22:50:50 -0400

    add icons buttons in whoiam

--- src/modules/who-am-i/WhoAmI.tsx
+++ src/modules/who-am-i/WhoAmI.tsx
@@ -5,32 +5,49 @@ const primaryFontStyle =
 
 function WhoAmI() {
   return (
-    <div className="h-screen min-h-min bg-dark flex justify-center items-center flex-col">
-      <div className="text-center">
-        <div className="text-lighter text-3xl font-medium text-3xl font-body mb-10">
-          Hello I am
-        </div>
-        <div className="text-lighter text-3xl font-medium text-6xl font-body mb-10">
-          Leonardo Sciretta
-        </div>
-        <Typewriter
-          onInit={(typewriter) => {
-            typewriter
+    <div className="h-screen min-h-min flex justify-center items-center flex-col text-center">
+      <div className="text-lighter text-3xl font-medium text-3xl font-body mb-10">
+        Hello I am
+      </div>
+      <div className="text-lighter text-3xl font-medium text-6xl font-body mb-10">
+        Leonardo Sciretta
+      </div>
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
 
-              .typeString(
-                `<span style="${primaryFontStyle}">TypeScript, JavaScript ...</span>`,
-              )
-              .pauseFor(3000)
-              .deleteAll(130)
-              .typeString(
-                `<span style="${primaryFontStyle}">Web and Mobile developer.</span>`,
-              )
-              .start()
-          }}
-          options={{
-            cursor: `<span style="${primaryFontStyle}">|</span>`,
-          }}
-        />
+            .typeString(
+              `<span style="${primaryFontStyle}">TypeScript, JavaScript ...</span>`,
+            )
+            .pauseFor(3000)
+            .deleteAll(130)
+            .typeString(
+              `<span style="${primaryFontStyle}">Web and Mobile developer.</span>`,
+            )
+            .start()
+        }}
+        options={{
+          cursor: `<span style="${primaryFontStyle}">|</span>`,
+        }}
+      />
+      <div className="mt-5 flex justify-center items-center w-10/12 h-20">
+        <button onClick={() => console.log('github')}>
+          <img className="w-12 h-12 sepia-0" src="./github.svg" alt="Github" />
+        </button>
+        <button onClick={() => console.log('gmail')}>
+          <img
+            className="ml-6 mr-6 w-12 h-12 sepia-0"
+            src="./gmail.svg"
+            alt="Github"
+          />
+        </button>
+        <button style={{ zIndex: 1 }} onClick={() => console.log('linkedin')}>
+          <img
+            className="w-12 h-12 sepia-0"
+            src="./linkedin.svg"
+            alt="Github"
+          />
+        </button>
       </div>
     </div>
   )
