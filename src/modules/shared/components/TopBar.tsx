b1cb9b3ddf107fbb801724dcb985933820ecaaf9
sciretta <sciretta28@gmail.com>
2022-01-13 20:04:01 -0400

    use avatar

--- src/modules/shared/components/TopBar.tsx
+++ src/modules/shared/components/TopBar.tsx
@@ -4,11 +4,7 @@ const navAnchorClass =
 function TopBar() {
   return (
     <nav className="fixed w-full backdrop-blur-sm flex items-center justify-center h-16 pl-10 pr-10 sm:justify-between">
-      <img
-        className="h-8 w-8 rounded-full"
-        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        alt=""
-      />
+      <img className="h-8 w-8 rounded-full" src="./avatar.png" alt="" />
       <div className=" hidden sm:block rounded-full focus:ring-white">
         <div className="flex space-x-4">
           <a href="#" className={navAnchorClass} aria-current="page">
