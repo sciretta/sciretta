55da537ec0691df5db4e8554fa5a5c9e6aee8f5b
sciretta <sciretta28@gmail.com>
2022-01-13 22:51:03 -0400

    skills component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import TopBar from 'src/modules/shared/components/TopBar'
+import Skills from 'src/modules/skills/Skills'
 import WhoAmI from 'src/modules/who-am-i/WhoAmI'
 // const Main = loadable(() => import('views/Main'))
 // <Main fallback={<Loading display="main section 👋" />} />
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-dark">
       <TopBar />
       <WhoAmI />
-    </>
+      <Skills />
+    </div>
   )
 }
---
+++ src/modules/skills/Skills.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Skills() {
+  return <div style={{ minHeight: '100vh' }}></div>
+}
+
+export default Skills
