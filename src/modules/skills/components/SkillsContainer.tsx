946ff2b1b6065689a03e25ef55e5d8f2777084af
Leonardo <sciretta28@gmail.com>
2022-01-16 21:27:17 -0400

    do down skills container

--- src/modules/skills/components/SkillsContainer.tsx
+++ src/modules/skills/components/SkillsContainer.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import SkillCard from './SkillCard'
 
 function SkillsContainer() {
+  const containerRef = useRef<HTMLDivElement>(null)
   return (
     <div
+      ref={containerRef}
       className="h-[600px] w-[340px] md:h-[900px] md:w-[380px] m-5 overflow-y-auto rounded-lg flex flex-col justify-start items-center pt-5"
       style={{ border: '1px solid yellow' }}
     >
@@ -16,6 +18,17 @@ function SkillsContainer() {
         <SkillCard />
         <SkillCard />
       </div>
+      <button
+        onClick={() =>
+          containerRef.current?.scrollBy({
+            top: 300,
+            behavior: 'smooth',
+          })
+        }
+        className="sticky bottom-5 opacity-20 z-10 bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+      >
+        <img src="./down-arrow.svg" className="h-8 w-8" alt="Down" />
+      </button>
     </div>
   )
 }
