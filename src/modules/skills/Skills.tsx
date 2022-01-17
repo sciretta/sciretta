fee7b8034c7d40f8bd82e581d7329e61b4a7c08d
Leonardo <sciretta28@gmail.com>
2022-01-16 22:14:24 -0400

    install react- beautiful-dnd

--- src/modules/skills/Skills.tsx
+++ src/modules/skills/Skills.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 import SkillsContainer from './components/SkillsContainer'
+
+const containers = ['General', 'Frontend', 'Backend']
 
 function Skills() {
   return (
@@ -8,9 +10,20 @@ function Skills() {
         Skills
       </div>
       <div className="min-h-screen flex flex-col items-center justify-evenly xl:flex-row">
-        <SkillsContainer />
-        <SkillsContainer />
-        <SkillsContainer />
+        <DragDropContext onDragEnd={(result) => console.log({ result })}>
+          <Droppable droppableId="skills-container">
+            {(droppableProvided) => (
+              <div
+                {...droppableProvided.droppableProps}
+                ref={droppableProvided.innerRef}
+              >
+                {containers.map((name, idx) => (
+                  <SkillsContainer key={name} name={name} idx={idx} />
+                ))}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
     </div>
   )
