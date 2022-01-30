cf6183c90924f2e83a0eb2132160766e6f7d8dc3
sciretta <sciretta28@gmail.com>
2022-01-30 09:48:18 -0400

    send email

--- src/modules/contact/Contact.tsx
+++ src/modules/contact/Contact.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { SendEmailData, SendEmailResponse } from '../shared/types'
 // import { useOnScreen } from '../shared/hooks'
 
 // const variants = {
@@ -12,10 +13,14 @@ const inputClass =
 
 // tailwind disabled button
 const disabledButtonClass =
-  'text-light bg-lighter hover:bg-lighter cursor-not-allowed'
+  'text-light bg-lighter cursor-not-allowed hover:bg-lighter'
 
 function Contact() {
-  const [isSending, setIsSending] = useState(false)
+  const [disabledButton, setDisabledButton] = useState(false)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [error, setError] = useState('')
   // const [isOpen, setIsOpen] = useState(false)
   // const ref = useRef<HTMLDivElement>(null)
   // const isIntersecting = useOnScreen(ref)
@@ -23,6 +28,47 @@ function Contact() {
   // useEffect((): void => {
   //   setIsOpen(isIntersecting)
   // }, [isIntersecting])
+
+  useEffect((): void => {
+    if (name && email && message) {
+      return setDisabledButton(false)
+    }
+    setDisabledButton(true)
+  }, [name, email, message])
+
+  function sendEmail() {
+    if (disabledButton) return
+    const data: SendEmailData = {
+      name,
+      email,
+      message,
+    }
+
+    fetch('/api/send-email', {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => res.json())
+      .then((res: SendEmailResponse) => {
+        if (res.success) {
+          // setSuccess(true)
+          setEmail('')
+          setMessage('')
+          setName('')
+        } else {
+          setError(res.message)
+        }
+      })
+      .catch((err) => {
+        setError(err)
+        setTimeout(() => {
+          setError('')
+        }, 5000)
+      })
+  }
 
   return (
     <div
@@ -32,31 +78,39 @@ function Contact() {
         Contact
       </span>
       <div className="h-[75vh] min-h-[500px] flex justify-center lg:justify-between p-16">
-        <div className="flex flex-col items-center min-h-fit bg-darker rounded-xl w-1/2 min-w-[300px] p-8">
+        <div className="flex flex-col items-center h-fit bg-darker rounded-xl w-1/2 min-w-[300px] p-8">
           <div className="w-full flex-col md:flex-row flex items-center justify-between mb-8">
             <input
+              onChange={(e): void => setName(e.target.value)}
               className={`${inputClass} md:mr-2`}
               type="text"
               placeholder="Name"
             />
             <input
+              onChange={(e): void => setEmail(e.target.value)}
               className={`${inputClass} mt-2 md:mt-0`}
               type="email"
               placeholder="Email"
             />
           </div>
           <textarea
+            onChange={(e): void => setMessage(e.target.value)}
             placeholder="Message"
             rows={10}
             className={`${inputClass} h-auto resize-none`}
           />
           <button
             className={`mt-10 hover:bg-light transition duration-150 ease-in-out text-darker font-body font-medium rounded-md bg-lighter p-4 ${
-              isSending ? disabledButtonClass : ''
+              disabledButton ? disabledButtonClass : ''
             }`}
-            onClick={() => setIsSending((prev) => !prev)}>
+            onClick={sendEmail}>
             Send email
           </button>
+          {error && (
+            <span className="flex text-red-500 font-medium text-3xl font-body mt-2 justify-center">
+              {error}
+            </span>
+          )}
         </div>
         <div className="min-h-48 hidden lg:flex flex-col w-1/2 px-8">
           <span className="flex text-lighter font-medium text-3xl font-body mb-10 justify-center">
--- src/modules/shared/types.ts
+++ src/modules/shared/types.ts
@@ -1,1 +1,12 @@
 export type TailwindBreakpoints = 'sm' | 'md' | 'lg' | 'xl' | '2xl'
+
+export type SendEmailResponse = {
+  success: boolean
+  message: string
+}
+
+export type SendEmailData = {
+  name: string
+  email: string
+  message: string
+}
