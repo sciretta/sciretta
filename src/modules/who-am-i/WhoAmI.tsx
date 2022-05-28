c9a4f50763ff350f9a6931af91f8945343fc573f
sciretta <sciretta28@gmail.com>
2022-05-28 19:39:04 -0400

    whoami 3d effect

--- src/modules/who-am-i/WhoAmI.tsx
+++ src/modules/who-am-i/WhoAmI.tsx
@@ -2,6 +2,7 @@ import { motion } from 'framer-motion'
 import router from 'next/router'
 import { useEffect, useState } from 'react'
 import Typewriter from 'typewriter-effect'
+import Tilt from 'react-parallax-tilt'
 
 const variants = {
   open: { opacity: 1, y: 0 },
@@ -21,32 +22,60 @@ function WhoAmI() {
     <div
       id="whoami"
       className="h-screen min-h-min flex justify-center items-center flex-col text-center">
-      <motion.div
-        variants={variants}
-        animate={isOpen ? 'open' : 'closed'}
-        initial={{ opacity: 0 }}
-        transition={{ type: 'just', delay: 0.6 }}
-        className="text-lighter text-3xl font-medium font-body">
-        <span className="mb-10 block">Hello I am</span>
-        <span className="block text-lighter font-medium text-6xl font-body mb-10">
-          Leonardo Sciretta
-        </span>
-        <Typewriter
-          onInit={(typewriter) => {
-            typewriter
-              .pauseFor(2000)
-              .typeString(`<span">JavaScript, TypeScript  ...</span>`)
-              .pauseFor(3000)
-              .deleteAll(20)
-              .typeString(`<span">Web and Mobile developer.</span>`)
-              .start()
-          }}
-          options={{
-            cursor: `<span">|</span>`,
-          }}
-        />
-        <span className="block mb-10" />
-      </motion.div>
+      <Tilt
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '50vw',
+          minWidth: 300,
+          transformStyle: 'preserve-3d',
+        }}
+        perspective={900}
+        glareEnable={true}
+        glareBorderRadius="10px"
+        glareMaxOpacity={0.45}
+        scale={1.02}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '35px',
+            fontStyle: 'italic',
+            color: 'white',
+            transform: 'translateZ(60px)',
+          }}>
+          <motion.div
+            variants={variants}
+            animate={isOpen ? 'open' : 'closed'}
+            initial={{ opacity: 0 }}
+            transition={{ type: 'just', delay: 0.6 }}
+            className="text-lighter text-3xl font-medium font-body">
+            <span className="mb-10 block">Hello I am</span>
+            <span className="block text-lighter font-medium text-6xl font-body mb-10">
+              Leonardo Sciretta
+            </span>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .pauseFor(2000)
+                  .typeString(`<span">JavaScript, TypeScript  ...</span>`)
+                  .pauseFor(3000)
+                  .deleteAll(20)
+                  .typeString(`<span">Web and Mobile developer.</span>`)
+                  .start()
+              }}
+              options={{
+                cursor: `<span">|</span>`,
+              }}
+            />
+            <span className="block mb-10" />
+          </motion.div>
+        </div>
+      </Tilt>
       <motion.div
         variants={variants}
         animate={isOpen ? 'open' : 'closed'}
