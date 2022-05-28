43aca2572e0c96095688ac73725e94932d6d317e
sciretta <sciretta28@gmail.com>
2022-05-28 19:56:52 -0400

    avoid automatic translations

---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: any) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="google" content="notranslate" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
