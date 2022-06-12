7b3529cb8ba6a1fcfd54ecb8dcacf21aad68c9ab
sciretta <sciretta28@gmail.com>
2022-06-11 21:54:19 -0400

    show portfolio apps on devices

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../src/styles/global.css'
+import '../node_modules/react-device-frameset/dist/styles/marvel-devices.min.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/modules/experience/Experience.tsx
+++ src/modules/experience/Experience.tsx
@@ -1,21 +1,50 @@
 import { motion } from 'framer-motion'
 import { useEffect, useRef, useState } from 'react'
-import Typewriter from 'typewriter-effect'
+import { DeviceFrameset } from 'react-device-frameset'
 import { useOnScreen } from '../shared/hooks'
 
-const variants = {
-  open: { opacity: 1, y: 0 },
-  closed: { opacity: 0, y: '40px' },
+const variantsRight = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: '400px' },
+}
+
+const variantsLeft = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: '-400px' },
 }
 
 function Experience() {
-  const [isOpen, setIsOpen] = useState(false)
-  const ref = useRef<HTMLDivElement>(null)
-  const isIntersecting = useOnScreen(ref)
+  const [isOpenFisherman, setIsOpenFisherman] = useState(false)
+  const refFisherman = useRef<HTMLDivElement>(null)
+  const isIntersectingFisherman = useOnScreen(refFisherman)
 
   useEffect((): void => {
-    setIsOpen(isIntersecting)
-  }, [isIntersecting])
+    setIsOpenFisherman(isIntersectingFisherman)
+  }, [isIntersectingFisherman])
+
+  const [isOpenLeadVolt, setIsOpenLeadVolt] = useState(false)
+  const refLeadVolt = useRef<HTMLDivElement>(null)
+  const isIntersectingLeadVolt = useOnScreen(refLeadVolt)
+
+  useEffect((): void => {
+    setIsOpenLeadVolt(isIntersectingLeadVolt)
+  }, [isIntersectingLeadVolt])
+
+  const [isOpenResident, setIsOpenResident] = useState(false)
+  const refResident = useRef<HTMLDivElement>(null)
+  const isIntersectingResident = useOnScreen(refResident)
+
+  useEffect((): void => {
+    setIsOpenResident(isIntersectingResident)
+  }, [isIntersectingResident])
+
+  const [isOpenCuplr, setIsOpenCuplr] = useState(false)
+  const refCuplr = useRef<HTMLDivElement>(null)
+  const isIntersectingCuplr = useOnScreen(refCuplr)
+
+  useEffect((): void => {
+    setIsOpenCuplr(isIntersectingCuplr)
+  }, [isIntersectingCuplr])
 
   return (
     <div
@@ -24,51 +53,63 @@ function Experience() {
       <span className="text-lighter font-medium text-5xl font-body mb-10 flex justify-center">
         Experience
       </span>
-      <div className="flex justify-between flex-col md:flex-row">
-        <div className="flex md:hidden w-full flex-col md:flex-row items-center mb-10">
-          <span className=" text-center text-lighter font-medium text-3xl font-body mr-2">
-            CobuildLab
-          </span>
-          <span className="text-center text-lighter font-medium text-xl font-body ">
-            since december 2020.
-          </span>
-        </div>
-        <div
-          onClick={() => open('https://www.cobuildlab.com/')}
-          className={`m-4 min-h-[300px] min-w-[240px] md:h-[470px] md:w-[400px] lg:h-[570px] lg:w-[500px] bg-center md:bg-top bg-[url('/cobuildLab.png')] bg-contain bg-no-repeat px-10 hover:cursor-pointer`}
-        />
-        <div ref={ref} className="flex flex-col w-full p-8">
-          <div className="hidden md:flex w-full flex-col md:flex-row items-center mb-10">
-            <span className=" text-center text-lighter font-medium text-3xl font-body mr-2">
-              CobuildLab
-            </span>
-            <span className="text-center text-lighter font-medium text-xl font-body ">
-              since december 2020.
-            </span>
-          </div>
-
-          <motion.div
-            variants={variants}
-            animate={isOpen ? 'open' : 'closed'}
-            initial={{ opacity: 0 }}
-            transition={{ type: 'just', delay: 0.6 }}
-            className="flex w-full md:w-4/6 text-start text-lighter font-medium text-base font-body">
-            <Typewriter
-              onInit={(typewriter) => {
-                typewriter
-                  .pasteString(
-                    "CobuildLab is a company located in Miami, FL, that specializes in the development of web and mobile CRM's, CMS's and e-commerce's aplications.<br/>Using tecnologies such as React, NodeJS, GraphQL, NextJS, Auth0, MongoDB, Firebase, 8base, Twilio and more, I have learned how to build buisness solutions that are scalable and secure.",
-                    null,
-                  )
-                  .start()
-              }}
-              options={{
-                cursor: `<span>|</span>`,
-              }}
-            />
-          </motion.div>
-        </div>
-      </div>
+      <motion.div
+        className="flex items-center justify-center"
+        ref={refFisherman}
+        variants={variantsRight}
+        animate={isOpenFisherman ? 'open' : 'closed'}
+        initial={{ opacity: 0 }}
+        transition={{ type: 'just', delay: 0.6 }}>
+        <DeviceFrameset device="MacBook Pro" zoom={0.6}>
+          <img src="/fisherman-landing.png" alt="fisherman" />
+        </DeviceFrameset>
+      </motion.div>
+      <motion.div
+        className="flex items-center justify-center"
+        ref={refLeadVolt}
+        variants={variantsLeft}
+        animate={isOpenLeadVolt ? 'open' : 'closed'}
+        initial={{ opacity: 0 }}
+        transition={{ type: 'just', delay: 0.6 }}>
+        <DeviceFrameset device="MacBook Pro" zoom={0.6}>
+          <img
+            style={{ height: '100%', width: '100%' }}
+            src="/leadvolt-landing.png"
+            alt="leadvolt"
+          />
+        </DeviceFrameset>
+      </motion.div>
+      <motion.div
+        className="flex items-center justify-center"
+        ref={refResident}
+        variants={variantsRight}
+        animate={isOpenResident ? 'open' : 'closed'}
+        initial={{ opacity: 0 }}
+        transition={{ type: 'just', delay: 0.6 }}>
+        <DeviceFrameset device="MacBook Pro" zoom={0.6}>
+          <img
+            style={{ height: '90%', width: '100%' }}
+            src="/resident-landing.png"
+            alt="resident"
+          />
+        </DeviceFrameset>
+      </motion.div>
+      <motion.div
+        className="flex items-center justify-center h-[700px]"
+        ref={refCuplr}
+        variants={variantsLeft}
+        animate={isOpenCuplr ? 'open' : 'closed'}
+        initial={{ opacity: 0 }}
+        transition={{ type: 'just', delay: 0.6 }}>
+        <DeviceFrameset device="iPhone X" height={750} width={370} zoom={0.5}>
+          <img
+            src="/cuplrwallet-landing.png"
+            width="100%"
+            height="100%"
+            alt="cuplr"
+          />
+        </DeviceFrameset>
+      </motion.div>
     </div>
   )
 }
